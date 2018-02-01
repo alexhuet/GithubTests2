ae40cef0a6df5b2ac577fb8f87b0ec8691583da2
alexhuet <36031254+alexhuet@users.noreply.github.com>
2018-02-01 10:38:25 +0100

    Delete test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    System.out.println("\n\n\n-----------------------------------------------------------------------------------------------------------------------------\n            COOKIFY\n\n\n");
-    System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
-    System.out.println("1. Prepare a recipe\n");
-    System.out.println("2. My Health\n");
-    System.out.println("3. Recipe Creator\n");
-    System.out.println("4. Recipe storage\n");
-    System.out.println("\n\n\n****Developpé par Arnaud Alex et Adrien ****\n****GG toto pour avoir trouvÃ© la strat du tableau de string****");
